602562f6fbb4a97f064b51f198659b021f4bff9f
QW3RT2U10PU <115784893+QW3RT2U10PU@users.noreply.github.com>
2026-01-10 20:41:04 +0100

    better player movement

--- src/entities/Player.cs
+++ src/entities/Player.cs
@@ -7,15 +7,19 @@ public partial class Player : CharacterBody2D
 
 	[Export]
 	public float Speed {get; set;} = 300.0f;
+
+	[Export]
+	public float Acceleration {get; set;} = 1000f;
+	
 	
 	[Export]
-	public float AirDrag {get; set;} = 1.7f;
-	
-	[Export]
-	public float DashSpeed {get; set;} = 5f;
+	public float DashSpeed {get; set;} = 3f;
 
 	[Export]
 	public float JumpStrength {get; set;} = 400.0f;
+
+	[Export] public double CoyoteTime {get; set;} = 0.08;
+	private double remainingCoyoteTime = 0;
 
 	private enum DashState {ACTIVE = 4, COOLDOWN = 1, NEEDS_FLOOR = 2}
 	private DashState dashState = 0;
@@ -25,10 +29,15 @@ public partial class Player : CharacterBody2D
 		GD.Print(dashState);
 		Timer dashTimer = GetNode<Timer>("dashTimer");
 		Timer dashCooldown = GetNode<Timer>("dashCooldown");
-		Vector2 velocity = Velocity;
 
 		// Add the gravity.
-		Vector2 direction = Input.GetVector("move_left", "move_right", "jump", "move_down");
+		Vector2 direction = Vector2.Zero;
+		if (Input.IsActionPressed("move_right")) direction.X++;
+		if (Input.IsActionPressed("move_left")) direction.X--;
+		if (Input.IsActionPressed("move_down")) direction.Y++;
+		if (Input.IsActionPressed("jump")) direction.Y--;
+		
+		Vector2 velocity = Velocity;
 		velocity.X = direction.X * Speed;
 		
 		if(Input.IsActionJustPressed("dash") && dashState == 0) {
@@ -36,30 +45,41 @@ public partial class Player : CharacterBody2D
 			dashCooldown.Start();
 			dashState = DashState.ACTIVE;
 		}
-
-		if(dashState == DashState.ACTIVE)
-		{
-			velocity.X *= DashSpeed;
-		}
 		
 		if (IsOnFloor())
 		{
+			remainingCoyoteTime = CoyoteTime;
 			if (dashState != DashState.ACTIVE) dashState &= ~DashState.NEEDS_FLOOR;
 		}
-		else if (dashState != DashState.ACTIVE)
-		{
-			velocity += GetGravity() * (float)delta;
-			velocity.X /= AirDrag;
-		}
-		else
-		{
-			velocity.Y = 0;
+		else {
+			remainingCoyoteTime -= delta;
+			if (dashState != DashState.ACTIVE)
+			{
+				velocity += GetGravity() * (float)delta;
+			}
+			else
+			{
+				velocity.Y = 0;
+			}
 		}
 		
-		if (direction.Y < 0 && IsOnFloor())
+		if (direction.Y < 0 && remainingCoyoteTime > 0)
 		{
 			velocity.Y = -JumpStrength;
 		}
+
+
+		if (dashState == DashState.ACTIVE)
+		{
+			velocity.X *= DashSpeed;
+		}
+		else
+		{
+			float xVelocityDelta = velocity.X - Velocity.X;
+			xVelocityDelta = Math.Sign(xVelocityDelta) * Math.Min(Math.Abs(xVelocityDelta), Acceleration * (float) delta);
+			velocity.X = Math.Clamp(Velocity.X + xVelocityDelta, -Speed, Speed);
+		}
+
 
 		Velocity = velocity;
 		MoveAndSlide();
