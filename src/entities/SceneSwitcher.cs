7bc7d0749af10eb674c17da7c2d28309bf08ada5
QW3RT2U10PU <115784893+QW3RT2U10PU@users.noreply.github.com>
2026-01-10 19:26:09 +0100

    scene switcher fix

--- src/entities/SceneSwitcher.cs
+++ src/entities/SceneSwitcher.cs
@@ -3,10 +3,10 @@ using Godot;
 [GlobalClass]
 public partial class SceneSwitcher : Interactable
 {
-	[Export] public PackedScene Loads {get; set;} = null;
+	[Export(PropertyHint.File, "*.scn, *.tscn")] public string Loads {get; set;} = null;
 
     public override void Interact(Node body)
     {
-        GetTree().ChangeSceneToPacked(Loads);
+        GetTree().ChangeSceneToFile(Loads);
     }
 }
